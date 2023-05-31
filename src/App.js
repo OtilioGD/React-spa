80555af6839424de0ac80b71277064cd48e3f3b8
OOGD <oogd17@gmail.com>
2023-05-31 11:53:42 -0600

    Creando Page404

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
 import  Home from './pages/Home';
 import Sobre from './pages/Sobre';
+import Page404 from './pages/Page404';
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/sobre' element={<Sobre />}/>
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
   );
